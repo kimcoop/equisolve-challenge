403fd16612e054b8bf5147fd7e6e7f9713a9e5d5
Kim Cooperrider <kim.cooperrider@gmail.com>
2015-09-17 20:02:13 -0700

    Styles in place

--- public/scripts.js
+++ public/scripts.js
@@ -81,22 +81,32 @@ define('constants',[],function() {
 require(['constants', 'jquery', 'tinyPubSub'], function(Constants) {
   'use strict';
 
+  var isQuerying = false;
+
   $(function() {
 
     $.publish('pr:query', {
       offset: 0,
-      limit: 5
+      limit: 10
     }); // Fire initial query
 
     $(window).scroll(function() {
       var $window = $(window);
+      if (isQuerying) {
+        return;
+      }
       if ($window.scrollTop() + $window.height() > $(document).height() - Constants.SCROLL_BUFFER_PX) {
         $.publish('pr:query', {
           offset: $('.pr-item').length
         });
+        isQuerying = true;
       }
     });
 
+  });
+
+  $.subscribe('pr:jsonLoadSuccess', function(event, data) {
+    isQuerying = false;
   });
 
 });
